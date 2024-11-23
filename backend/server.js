0577d54d751ef31ae9f39c92d130e88aca78e71a
Killian <martykillian7@gmail.com>
2024-11-23 14:11:53 +0100

    Connecting to the MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.get("/products", (req,res) => {
-
-});
+app.get("/products", (req,res) => {});
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 });
